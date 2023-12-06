9e1a2495738dcd954f20978c001fded3f790e24f
André Dias <andredias.us@gmail.com>
2023-12-05 21:25:47 -0300

    Displaying results inside the page

--- main.js
+++ main.js
@@ -23,8 +23,7 @@ function getReturnType(url) {
 }
 
 function checkUrl(url) {
-  console.log('🚀 ~ file: main.js:26 ~ checkUrl ~ url:', url);
-  const urlExists = Math.random() < 0.5;
+  const urlExists = Math.random() < 0.55;
   const returnType = urlExists ? getReturnType(url) : null;
 
   const response = {
@@ -38,23 +37,46 @@ function checkUrl(url) {
   return response;
 }
 
+const setResult = (result) => {
+  const { status, data } = result;
+  const { urlExists, returnType, reason } = data;
+
+  const title = document.getElementById('title');
+  const body = document.getElementById('body');
+
+  if (status === 'fail') {
+    resultSection.classList.remove('valid');
+    resultSection.classList.add('invalid');
+    title.innerText = status;
+    body.innerText = reason;
+    return;
+  }
+
+  resultSection.classList.remove('invalid');
+  resultSection.classList.add('valid');
+
+  const bodyText = urlExists
+    ? `URL exists, returning a ${returnType}!`
+    : "Although valid, this URL doesn't exists.";
+
+  title.innerText = 'Valid URL';
+  body.innerText = bodyText;
+};
+
 const debouncedCheck = debouncePromise(checkUrl, 500);
 
 const handleInput = async ({ target }) => {
   const { value, validity } = target;
 
   if (!validity.valid) {
-    resultSection.classList.remove('valid');
-    resultSection.classList.add('invalid');
+    setResult({ status: 'fail', data: { reason: 'Invalid URL format.' } });
     return;
   }
-  resultSection.classList.remove('invalid');
-  resultSection.classList.add('valid');
 
   resultSection.classList.add('loading');
   debouncedCheck(value).then((result) => {
     resultSection.classList.remove('loading');
-    console.log(result);
+    setResult(result);
   });
 };
 
